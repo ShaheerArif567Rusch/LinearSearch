f927fdfb8167683cb2389c91a7001f2ea63537f5
ShaheerArif567Rusch <112656881+ShaheerArif567Rusch@users.noreply.github.com>
2022-12-06 13:01:20 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-static int linearSearchInt(List<int> anArray, int item) {
-    for (int i = 0; i < anArray.Count; i++) {
-         if (anArray[i] == item) {
-             return i; 
-    }
-    }
-    return -1;
-    
-} 
 
-static int linearSearchStr(List<string> anArray, string item) {
-    // Test Code
-    for (int i = 0; i < anArray.Count; i++) {
-        if (anArray[i] == item) {
-            return i; 
+
+static int BinarySearchInt(List<int> anArray, int item) {
+  
+    int LowerIndex = 0; 
+    int UpperIndex = anArray.Count - 1; 
+    while (LowerIndex <= UpperIndex) { 
+        double mid = (LowerIndex + UpperIndex) / 2;
+        int MiddleIndex = (int)Math.Floor(mid); 
+        
+        if (item == anArray[MiddleIndex]) {
+            return MiddleIndex; 
+        } else if (item < anArray[MiddleIndex]) {
+            UpperIndex = MiddleIndex - 1; 
+        } else {
+            LowerIndex = MiddleIndex + 1; 
         }
     }
-    return -1;
+    return -1; 
 }
 
+static int BinarySearchStr(List<string> anArray, string item) {
+    int LowerIndex = 0; 
+    int UpperIndex = anArray.Count - 1; 
+    while (LowerIndex <= UpperIndex) {
+        double mid = (LowerIndex + UpperIndex) / 2; 
+        int MiddleIndex = (int)Math.Floor(mid); 
+    
+        int compare = String.Compare(item, anArray[MiddleIndex]); 
+        if (compare == 0) {
+            return MiddleIndex; 
+        } else if (compare < 0) {
+            UpperIndex = MiddleIndex - 1; 
+        } else {
+            LowerIndex = MiddleIndex + 1; 
+        }
+    }
+    return -1;    
 
-
-
-List<string> words = new List<string>(){"Pandas", "Thomas", "Destruction", "Snakes", "Water", "Iphone"}; 
-List<string> vowels = new List<string>(){"a", "e", "i", "o", "u"};
-List<int> odds = new List<int>(){1, 3, 5, 7, 9, 11, 13, 15, 17}; 
-List<int> evens = new List<int>(){2, 4, 6, 8, 10, 12, 14}; 
-
-
-
-int index1 = linearSearchInt(odds, 53);
+}
+List<int> numbers = new List<int>(){10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; 
+int index1 = BinarySearchInt(numbers, 40); 
 Console.WriteLine(index1);
 
-int index2 = linearSearchInt(evens, 4);
+List<int> perfectSquares = new List<int>(){1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 225}; 
+int index2 = BinarySearchInt(perfectSquares, 122); 
 Console.WriteLine(index2);
+List<string> words1 = new List<string>(){"Thomas", "Gordon", "Percy", "Edward", "Henry", "Toby", "Emily"}; 
+int index3 = BinarySearchStr(words1, "Gordon"); 
+Console.WriteLine(index3); 
+int index4 = BinarySearchStr(words1, "Spencer"); 
+Console.WriteLine(index4); 
+List<string> Phones = new List<string>(){"IPhone1", "IPhone2", "IPhone3", "IPhone4", "IPhone5SE", "IPhone6s+", "IPhone7", "IPhone8", "IPhoneX", "IPhoneXS", "IPhoneXR", "IPhoneXSMax", "IPhone11", "IPhone11Pro", "IPhone11ProMax"}; 
+int index5 = BinarySearchStr(Phones, "IPhone7"); 
+Console.WriteLine(index5); 
+int index6 = BinarySearchStr(Phones, "IPhoneXS"); 
+Console.WriteLine(index6); 
+int index7 = BinarySearchStr(Phones, "IPhone999"); 
+Console.WriteLine(index7); 
 
-int index3 = linearSearchStr(words, "hi");
-Console.WriteLine(index3);
 
-int index4 = linearSearchStr(vowels, "e");
-Console.WriteLine(index4);
+
+
+
 
 
 
